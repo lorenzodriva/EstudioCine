7f80de5ceba3ebea8071772920c68a4080cb02fe
Riva <lriva@FRSN.UTN.EDU.AR>
2024-09-19 11:58:55 -0300

    nuevo actor

--- EstudioCine/Program.cs
+++ EstudioCine/Program.cs
@@ -11,6 +11,7 @@ class Program
         Actor actor2 = new Actor("Guillermo Francella");
         Actor actor3 = new Actor("Adrian Suar");
         Actor actor4 = new Actor("Dario Grandinetti");
+        Actor actor5 = new Actor("Adam Sandler");
         List<Actor> listaActores = new List<Actor>();
         listaActores.Add(actor1);
         listaActores.Add(actor4);
